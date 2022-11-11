a9dd7cf4ecd01fc1dcc12344f61b6cf6a30d295b
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2022-11-11 13:29:21 +0100

    User controller: implemented

--- lab-6/auth-ws/src/main/java/it/unibo/ds/ws/users/impl/UserControllerImpl.java
+++ lab-6/auth-ws/src/main/java/it/unibo/ds/ws/users/impl/UserControllerImpl.java
@@ -4,8 +4,10 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.HttpResponseException;
 import it.unibo.ds.ws.AbstractController;
+import it.unibo.ds.ws.User;
 import it.unibo.ds.ws.users.UserApi;
 import it.unibo.ds.ws.users.UserController;
+import it.unibo.ds.ws.utils.Filters;
 
 public class UserControllerImpl extends AbstractController implements UserController {
 
@@ -19,31 +21,55 @@ public class UserControllerImpl extends AbstractController implements UserContro
 
     @Override
     public void getAllUserNames(Context context) throws HttpResponseException {
-        throw new Error("not implemented");
+        UserApi api = getApi(context);
+        var futureResult = api.getAllNames(
+                getOptionalIntParam(context, "skip", 0),
+                getOptionalIntParam(context, "limit", 10),
+                getOptionalStringParam(context, "filter", "")
+        );
+        asyncReplyWithBody(context, "application/json", futureResult);
     }
 
     @Override
     public void postNewUser(Context context) throws HttpResponseException {
-        throw new Error("not implemented");
+        UserApi api = getApi(context);
+        var user = context.bodyAsClass(User.class);
+        var futureResult = api.registerUser(user);
+        asyncReplyWithBody(context, "application/json", futureResult);
     }
 
     @Override
     public void deleteUser(Context context) throws HttpResponseException {
-        throw new Error("not implemented");
+        UserApi api = getApi(context);
+        var userId = context.pathParam("userId");
+        var futureResult = api.removeUser(userId);
+        asyncReplyWithoutBody(context, "application/json", futureResult);
     }
 
     @Override
     public void getUser(Context context) throws HttpResponseException {
-        throw new Error("not implemented");
+        UserApi api = getApi(context);
+        var userId = context.pathParam("userId");
+        var futureResult = api.getUser(userId);
+        asyncReplyWithBody(context, "application/json", futureResult);
     }
 
     @Override
     public void putUser(Context context) throws HttpResponseException {
-        throw new Error("not implemented");
+        UserApi api = getApi(context);
+        var userId = context.pathParam("userId");
+        var changes = context.bodyAsClass(User.class);
+        var futureResult = api.editUser(userId, changes);
+        asyncReplyWithBody(context, "application/json", futureResult);
     }
 
     @Override
     public void registerRoutes(Javalin app) {
-        // TODO implement
+        app.before(path("*"), Filters.ensureClientAcceptsMimeType("application", "json"));
+        app.get(path("/"), this::getAllUserNames);
+        app.post(path("/"), this::postNewUser);
+        app.get(path("/{userId}"), this::getUser);
+        app.delete(path("/{userId}"), this::deleteUser);
+        app.put(path("/{userId}"), this::putUser);
     }
 }
